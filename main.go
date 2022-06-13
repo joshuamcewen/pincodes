06453dd2cf84603fb4a9ee0c28c9a37611156437
Josh McEwen <josh.mcewen@and.digital>
2022-06-13 21:21:38 +0100

    Use string instead of int for PINs with leading zeroes

--- main.go
+++ main.go
@@ -2,39 +2,28 @@ package pincodes
 
 import (
 	"sort"
-	"strconv"
 )
 
 // splitDigits parses an integer and returns an array of integers representing each digit.
-func splitDigits(number int) []int {
-	var digits []int
-	n := number
+func splitDigits(number string) []string {
+	var digits []string
 
-	if n == 0 {
-		digits = []int{0}
-	}
-
-	for n != 0 {
-		digits = append(digits, n%10)
-		n = n / 10
-	}
-
-	for i, j := 0, len(digits)-1; i < j; i, j = i+1, j-1 {
-		digits[i], digits[j] = digits[j], digits[i]
+	for _, digit := range number {
+		digits = append(digits, string(digit))
 	}
 
 	return digits
 }
 
-var keypad = [4][3]int{
-	{1, 2, 3},
-	{4, 5, 6},
-	{7, 8, 9},
-	{-1, 0, -1},
+var keypad = [4][3]string{
+	{"1", "2", "3"},
+	{"4", "5", "6"},
+	{"7", "8", "9"},
+	{" ", "0", " "},
 }
 
 // getIndexes take a keypad digit and returns the row/column indexes.
-func getIndexes(number int) (int, int) {
+func getIndexes(number string) (int, int) {
 	var rows, columns = len(keypad), len(keypad[0])
 	var r, c = 0, 0
 
@@ -55,20 +44,20 @@ func getIndexes(number int) (int, int) {
 }
 
 // getPossibleDigits takes a keypad digit and returns all possible selections.
-func getPossibleDigits(number int) []int {
-	var digits = []int{number}
+func getPossibleDigits(number string) []string {
+	var digits = []string{number}
 
 	var rows, columns = len(keypad), len(keypad[0])
 	var r, c = getIndexes(number)
 
 	// Left
-	if c-1 >= 0 && keypad[r][c-1] != -1 {
+	if c-1 >= 0 && keypad[r][c-1] != " " {
 		left := keypad[r][c-1]
 		digits = append(digits, left)
 	}
 
 	// Right
-	if c+1 <= columns-1 && keypad[r][c+1] != -1 {
+	if c+1 <= columns-1 && keypad[r][c+1] != " " {
 		right := keypad[r][c+1]
 		digits = append(digits, right)
 	}
@@ -80,7 +69,7 @@ func getPossibleDigits(number int) []int {
 	}
 
 	// Down
-	if r+1 <= rows-1 && keypad[r+1][c] != -1 {
+	if r+1 <= rows-1 && keypad[r+1][c] != " " {
 		down := keypad[r+1][c]
 		digits = append(digits, down)
 	}
@@ -88,10 +77,10 @@ func getPossibleDigits(number int) []int {
 	return digits
 }
 
-var digitsByWear = []int{5, 1, 2, 8, 7, 4, 0, 3, 6, 9}
+var digitsByWear = []string{"5", "1", "2", "8", "7", "4", "0", "3", "6", "9"}
 
 // getDigitPriority takes a number and generates a numeric priority based on wear.
-func getDigitPriority(number int) int {
+func getDigitPriority(number string) int {
 	digits := splitDigits(number)
 	priority := 0
 
@@ -107,9 +96,9 @@ func getDigitPriority(number int) int {
 }
 
 // getPermutations takes a keypad combination and returns all possible permutations, most likely first.
-func getPermutations(number int) []int {
+func getPermutations(number string) []string {
 	digits := splitDigits(number)
-	var combinations [][]int
+	var combinations [][]string
 
 	for _, digit := range digits {
 		combinations = append(combinations, getPossibleDigits(digit))
@@ -119,14 +108,13 @@ func getPermutations(number int) []int {
 
 	// For each array of digits...
 	for i := 1; i < len(combinations); i++ {
-		var updated []int
+		var updated []string
 
 		//	For each digit in that array...
 		for _, n := range combinations[i] {
 			// For each number already in the accumulator...
 			for _, a := range accumulator {
-				combination, _ := strconv.Atoi(strconv.Itoa(a) + strconv.Itoa(n))
-				updated = append(updated, combination)
+				updated = append(updated, a+n)
 			}
 		}
 
--- main_test.go
+++ main_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestSplitDigits(t *testing.T) {
 	var cases = []struct {
-		Number   int
-		Expected []int
+		Number   string
+		Expected []string
 	}{
-		{Number: 10, Expected: []int{1, 0}},
-		{Number: 0, Expected: []int{0}},
-		{Number: 1234, Expected: []int{1, 2, 3, 4}},
+		{Number: "10", Expected: []string{"1", "0"}},
+		{Number: "0", Expected: []string{"0"}},
+		{Number: "1234", Expected: []string{"1", "2", "3", "4"}},
 	}
 
 	for _, c := range cases {
@@ -29,13 +29,13 @@ func TestSplitDigits(t *testing.T) {
 
 func TestGetIndexes(t *testing.T) {
 	var cases = []struct {
-		Number   int
+		Number   string
 		Expected []int
 	}{
-		{Number: 1, Expected: []int{0, 0}},
-		{Number: 5, Expected: []int{1, 1}},
-		{Number: 9, Expected: []int{2, 2}},
-		{Number: 0, Expected: []int{3, 1}},
+		{Number: "1", Expected: []int{0, 0}},
+		{Number: "5", Expected: []int{1, 1}},
+		{Number: "9", Expected: []int{2, 2}},
+		{Number: "0", Expected: []int{3, 1}},
 	}
 
 	for _, c := range cases {
@@ -50,13 +50,13 @@ func TestGetIndexes(t *testing.T) {
 
 func TestGetPossibleDigits(t *testing.T) {
 	var cases = []struct {
-		Number   int
-		Expected []int
+		Number   string
+		Expected []string
 	}{
-		{Number: 1, Expected: []int{1, 2, 4}},
-		{Number: 5, Expected: []int{5, 4, 6, 2, 8}},
-		{Number: 9, Expected: []int{9, 8, 6}},
-		{Number: 0, Expected: []int{0, 8}},
+		{Number: "1", Expected: []string{"1", "2", "4"}},
+		{Number: "5", Expected: []string{"5", "4", "6", "2", "8"}},
+		{Number: "9", Expected: []string{"9", "8", "6"}},
+		{Number: "0", Expected: []string{"0", "8"}},
 	}
 
 	for _, c := range cases {
@@ -71,12 +71,12 @@ func TestGetPossibleDigits(t *testing.T) {
 
 func TestGetDigitPriority(t *testing.T) {
 	var cases = []struct {
-		Number   int
+		Number   string
 		Expected int
 	}{
-		{Number: 5, Expected: 10},
-		{Number: 9, Expected: 1},
-		{Number: 7, Expected: 6},
+		{Number: "5", Expected: 10},
+		{Number: "9", Expected: 1},
+		{Number: "7", Expected: 6},
 	}
 
 	for _, c := range cases {
@@ -91,12 +91,13 @@ func TestGetDigitPriority(t *testing.T) {
 
 func TestGetPermutations(t *testing.T) {
 	var cases = []struct {
-		Number   int
-		Expected []int
+		Number   string
+		Expected []string
 	}{
-		{Number: 46, Expected: []int{55, 15, 75, 45, 53, 56, 13, 59, 16, 19, 73, 43, 76, 46, 79, 49}},
-		{Number: 1, Expected: []int{1, 2, 4}},
-		{Number: 8, Expected: []int{5, 8, 7, 0, 9}},
+		{Number: "46", Expected: []string{"55", "15", "75", "45", "53", "56", "13", "59", "16", "19", "73", "43", "76", "46", "79", "49"}},
+		{Number: "1", Expected: []string{"1", "2", "4"}},
+		{Number: "8", Expected: []string{"5", "8", "7", "0", "9"}},
+		{Number: "01", Expected: []string{"81", "82", "01", "02", "84", "04"}},
 	}
 
 	for _, c := range cases {
@@ -110,7 +111,7 @@ func TestGetPermutations(t *testing.T) {
 }
 
 func TestGetPermutations97516(t *testing.T) {
-	permutations := getPermutations(97516)
+	permutations := getPermutations("97516")
 	json, _ := json.Marshal(permutations)
 
 	fmt.Printf("%s", json)
