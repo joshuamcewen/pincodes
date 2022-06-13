9a69018be8043bff0824af4f0a678f88b6595897
Josh McEwen <josh.mcewen@and.digital>
2022-06-13 19:03:57 +0100

    Implement splitDigits logic

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package pincodes
 
+// splitDigits parses an integer and returns an array of integers representing each digit.
 func splitDigits(number int) []int {
-	return []int{}
+	var digits []int
+	n := number
+
+	if n == 0 {
+		digits = []int{0}
+	}
+
+	for n != 0 {
+		digits = append(digits, n%10)
+		n = n / 10
+	}
+
+	return digits
 }
